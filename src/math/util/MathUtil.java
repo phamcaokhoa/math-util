cc2df0941d84d1d5e91c56ee206c0b5cb2cceef1
phamcaokhoa <khoapcse141096@fpt.edu.vn>
2020-11-12 08:37:03 +0700

    1st upload

---
+++ src/math/util/MathUtil.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author Khoa Pham
+ */
+public class MathUtil {
+
+    // class này chứa các hàm tiện ích dùng chung cho mọi nơi
+    //do đó nó phải nên là static
+    public static final double PI = 3.1415;
+    
+    // hàm tính n giai thừa . n>= 0 && n <= 20;
+    // nếu quá 20 tràn kiểu long
+    public static long getFactorial(int n){
+        if(n < 0 || n > 20){
+            throw new IllegalArgumentException("n must be >= 0 & <= 20");
+            // đưa giá trị sai, không nên trả về 1 giá trị ám chỉ sai
+        }if (n == 0 || n == 1)
+            return 1; //0! = 1, 1! = 1
+        long result = 1;
+        for (int i = 2; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+    
+    
+}
---
+++ src/math/util/main.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package math.util;
+
+/**
+ *
+ * @author Khoa Pham
+ */
+public class main {
+    public static void main(String[] args) {
+        //Test hàm = mắt
+        System.out.println("5!: " + MathUtil.getFactorial(5));
+        //expected:120,            actual:120
+        //Kì vọng kết quả trả về 120 khi gọi hàm getFactorial
+        
+        //Test bộ data khác, test case, tình huống khác
+        System.out.println("0!: " + MathUtil.getFactorial(0));
+        //expected:1,            actual:1
+        
+        
+        //e: ném về ngoại lệ IllegalArgumentException
+        System.out.println("a" + MathUtil.getFactorial(-1));
+        
+    }
+}
+
+        // Code viết ra thì Dev phải test trước 
+        // Có vài hình thức test code khác nhau
+
+        // 1.TDD, Test Driven Development
+        // Viết dàn khung của hàm getFactorial
+        // Sau đó dựng hàm test()
+        // Chạy thử coi xanh đỏ, đỏ nghĩa là lỗi, xanh nghĩa là ổn
+        // Cứ liên tục xanh đỏ như thế trong quá trình viết code
+        // và sửa code, ngta gọi là lập trình hướng theo thỏa mãn 
+        // việc test, TDD
+//Cách 2: sout(hàm kèm data) để coi kqua có đúng như mình kì vọng
+        //Nó phải trả về như thế hay không -> Xem kết quả test = mắt
+
+//Cách 3: JOPane của bên JavaDesktop để popup kết quả lên màn hình,
+        // Vẫn phải xem bằng mắt
+//Cách 4: Viết 1 trang web gọi hàm xử lí, ... Hao sức
+
+
+// -> Dùng cách 1 & 2
